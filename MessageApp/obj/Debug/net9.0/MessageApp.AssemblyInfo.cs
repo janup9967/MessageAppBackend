ea54ddc339105479c39415aa5f621ecabc3c86a5
Anup Jaiswal <anup-chhotelal.jaiswal@capgemini.com>
2025-08-13 17:42:45 +0530

    send message signalR

--- MessageApp/obj/Debug/net9.0/MessageApp.AssemblyInfo.cs
+++ MessageApp/obj/Debug/net9.0/MessageApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MessageApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+467519d15eceb6397d76ce2a1b36f40f7f169448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7345aadaa446396c3d7d041e5090354743484a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MessageApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MessageApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
