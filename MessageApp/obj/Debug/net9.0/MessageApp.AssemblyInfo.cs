8ead215ae9d9bdfbc00fc922d0c2babd26da73d2
pugliyamadhur-lab <pugliyamadhur@gmail.com>
2025-08-21 13:01:25 +0530

    sm, x;L

--- MessageApp/obj/Debug/net9.0/MessageApp.AssemblyInfo.cs
+++ MessageApp/obj/Debug/net9.0/MessageApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MessageApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be36b2f9d1ff467cc7f11bad2de93818805284be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258d4821cc74285f373ee79dbf691d1428c9ef25")]
 [assembly: System.Reflection.AssemblyProductAttribute("MessageApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MessageApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
